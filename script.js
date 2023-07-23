23f87e4b7cd3ff4137fa4ff4f65f79b348637e6c
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-07-23 15:25:16 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function tgl(){
     var t = document.getElementById("btn");
-    if (t.value == "Light mode") {
-      t.value = "Dark mode";
-      document.body.style.background = "black";
-      document.body.style.color = "white";
-      document.getElementsByTagName(a).style.color = "white";
-    } else if(t.value == "Dark mode") {
+    if (t.value == "Dark mode") {
       t.value = "Light mode";
+      document.body.style.background = "#171717";
+      document.body.style.color = "white";
+      
+    } else if(t.value == "Light mode") {
+      t.value = `Dark mode`;
       document.body.style.background = "white";
       document.body.style.color = "black";
-    
     }
   }
