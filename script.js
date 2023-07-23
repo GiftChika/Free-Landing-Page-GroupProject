b66d47574d70d2759822aed7bf71bea0b35f5cac
Finbarr11 <61949899+Finbarr11@users.noreply.github.com>
2023-07-23 14:00:44 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let scrollContainer = document.querySelector(".child2");
+let leftBtn = document.getElementById("leftBtn");
+let rightBtn = document.getElementById("rightBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+
+rightBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+leftBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
 
